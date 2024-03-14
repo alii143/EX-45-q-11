270e912785da229168fcc7f07fffedf5f640ad5f
Waqas Ali <160438548+alii143@users.noreply.github.com>
2024-03-15 04:17:15 +0500

    Add files via upload

---
+++ EX 45 q 11/main.js
@@ -0,0 +1,4 @@
+var members = ["talha", "hassan", "pervaiz", "umer"];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ EX 45 q 11/main.ts
@@ -0,0 +1,4 @@
+let members: string[] =[`talha`,`hassan`,`pervaiz`,`umer`]
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
